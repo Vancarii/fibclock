7e2186e784372ca17192caa93ada7c57bff3efd6
Vancarii <xnycwang@yahoo.com>
2025-04-09 14:13:59 -0700

    Initial Commit

---
+++ fibClock/fibClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  fibClockApp.swift
+//  fibClock
+//
+//  Created by Yecheng Wang on 2025-04-09.
+//
+
+import SwiftUI
+
+@main
+struct fibClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fibClockTests/fibClockTests.swift
@@ -0,0 +1,17 @@
+//
+//  fibClockTests.swift
+//  fibClockTests
+//
+//  Created by Yecheng Wang on 2025-04-09.
+//
+
+import Testing
+@testable import fibClock
+
+struct fibClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
